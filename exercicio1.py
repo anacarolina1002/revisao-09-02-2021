19a6f2b582cdb575080179e2447f4e2c6dcc273e
Anacarolina1002 <carolquaglioto@outlook.com>
2021-02-09 15:17:49 -0200

    :umbrella: tabuada

---
+++ exercicio1.py
@@ -0,0 +1,8 @@
+#Crie um programa que receba um numero inteiro de 1 ao 10 e imprima sua tabuada.
+def numeros():
+    multiplicando=int(input("Insira um número para que sua tabuada seja exibida: "))
+    for multiplicador in range(11):
+        resultado=multiplicando*multiplicador
+        print(multiplicando,"X",multiplicador,"=",resultado)
+numeros()
+
